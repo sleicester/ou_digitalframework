6d58c201bef18659950156d3d9225ee352900723
Stephen Creasey <stephen.creasey@open.ac.uk>
2014-03-06 14:48:34 +0000

    fix error in page.tpl.php

--- ou_digitalframework/templates/page.tpl.php
+++ ou_digitalframework/templates/page.tpl.php
@@ -78,13 +78,12 @@
  * @see html.tpl.php
  */
 ?>
-<<<<<<< HEAD
+
 <div id="int-site"><div id="page">
 <?php include base_path() . path_to_theme() . "/assets/header.html"; ?>
 
   <div class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
-=======
->>>>>>> b02434a55b5571df5f84f643e0acc1883729271c
+
 
 <div id="int-site" class="<?php print theme_get_setting('ou_df_colour_scheme'); ?>"><div id="page">
 <?php include base_path() . path_to_theme() . theme_get_setting('ou_df_path_to_header'); ?>
