77f154b8ef5b7187e646d032ca5d37c2e6009979
Stephen Creasey <stephen.creasey@open.ac.uk>
2014-03-11 12:39:32 +0000

    Added js file for accordion

---
+++ ou_digitalframework/js/df-accordion.js
@@ -0,0 +1,8 @@
+(function ($) {
+            //Accordion
+            $("#custom_id").accordion({
+                autoHeight: false,
+                clearStyle: true,
+                animate: 150
+            });
+        })(jQuery);
