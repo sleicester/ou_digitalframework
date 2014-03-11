e50c2d014badc6bef359f9bfe5f17d56b26ea979
Stephen Creasey <stephen.creasey@open.ac.uk>
2014-03-11 15:48:25 +0000

    Added js file for accordion

--- ou_digitalframework/js/df-accordion.js
+++ ou_digitalframework/js/df-accordion.js
@@ -1,8 +1,11 @@
-(function ($) {
-            //Accordion
-            $("#custom_id").accordion({
+(function($){
+  Drupal.behaviors.ou_digital_futures = {
+    attach: function(context, settings) {
+     $("#custom_id").accordion({
                 autoHeight: false,
                 clearStyle: true,
                 animate: 150
-            });
-        })(jQuery);
+              });
+    }
+  };
+})(jQuery);        
