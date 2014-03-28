8864d6a9c607209b7d00a6389f838be740a79d26
Stephen Creasey <stephen.creasey@open.ac.uk>
2014-03-28 14:58:24 +0000

    Added new block template to output l2/3 menu blocks and theme settings to go with

--- ou_digitalframework/theme-settings.php
+++ ou_digitalframework/theme-settings.php
@@ -18,6 +18,22 @@
 
   );
   
+  $form['ou_theme_settings']['ou_df_siblings_block_id'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Block ID to display as Siblings Menu Block'),
+    '#default_value' => theme_get_setting('ou_df_siblings_block_id'),
+    '#description' => t("The block ID that will be used for the Sibling menu block - (no content for this block is displayed)"),    
+
+  );
+  
+    $form['ou_theme_settings']['ou_df_children_block_id'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Block ID to display as Children Menu Block'),
+    '#default_value' => theme_get_setting('ou_df_children_block_id'),
+    '#description' => t("The block ID that will be used for the Children menu block - (no content for this block is displayed)"),    
+
+  );
+  
       $form['ou_theme_settings']['ou_df_colour_scheme'] = array(
     '#type' => 'select',
     '#title' => t('Required site colour scheme'),
