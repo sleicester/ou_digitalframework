cc1fb8fe10a7feb16e65361e3802ad43af43f1be
Stephen Creasey <stephen.creasey@open.ac.uk>
2014-04-01 12:44:43 +0100

    Added function to choose site wrapper to menu and function to hide top header nav

--- ou_digitalframework/js/hide-header-menu.js
+++ ou_digitalframework/js/hide-header-menu.js
@@ -1,7 +1,7 @@
 (function($){
   Drupal.behaviors.ou_digital_futures = {
     attach: function(context, settings) {
-$( "#ou-global-primary-navigation" ).hide();
+/*$( "#ou-global-primary-navigation" ).hide();
     }
-  };
+  };*/
 })(jQuery);
