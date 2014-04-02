401c9b6189c292a7fedcaf6461fa421dfd87bdbc
Stephen Creasey <stephen.creasey@open.ac.uk>
2014-04-02 11:43:39 +0100

    added int-container to block title template

--- ou_digitalframework/js/hide-header-menu.js
+++ ou_digitalframework/js/hide-header-menu.js
@@ -1,7 +1,7 @@
-/*(function($){
+(function($){
     Drupal.behaviors.ou_digital_futures = {
       attach: function(context, settings) {
         $("#ou-global-primary-navigation.ou-df-header-nav").hide();
       }
     }
-  })(jQuery);*/
+  })(jQuery);
