687ab306117552ca1b34262e2ee7a20585967b6f
Stephen Creasey <stephen.creasey@open.ac.uk>
2014-04-11 14:31:28 +0100

    Changes to theme-settings & html tpl

--- ou_digitalframework/templates/html.tpl.php
+++ ou_digitalframework/templates/html.tpl.php
@@ -78,14 +78,10 @@
 
 <div id="int-site">
 
-    <!--Added SC from LA 11-4-14 -->
 <script src="/oudigital/eep/assets/js/vendor/jquery-1.10.2.min.js"></script>
 
   <script src="/oudigital/eep/assets/js/vendor/jquery.placeholder.js"></script>
   <script src="/oudigital/eep/assets/js/vendor/jquery.smartresize.js"></script>
-  
-    <script> var $j = jQuery.noConflict();</script>
-  <script>
 
   <?php include theme_get_setting('ou_df_path_to_header'); ?>
 
@@ -96,7 +92,11 @@
   <?php include theme_get_setting('ou_df_path_to_footer') ; ?>
 </div>
 
+    <!--Added SC from LA 11-4-14 -->
 
+  
+    <script> var $j = jQuery.noConflict();</script>
+  <script>
    
   
     // Only hide if your CMS menu has been output correctly
