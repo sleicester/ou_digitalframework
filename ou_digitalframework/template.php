9db3ea5397d5897f0c773614dedcd9db83441933
Stephen Creasey <stephen.creasey@open.ac.uk>
2014-04-23 14:08:20 +0100

    Added fix for level 2 sticky navigation

--- ou_digitalframework/template.php
+++ ou_digitalframework/template.php
@@ -5,7 +5,6 @@ function ou_digital_futures_preprocess_html(&$variables) {
 
   // add new classes to body from theme settings
   $variables['classes_array'][] = theme_get_setting('ou_df_colour_scheme');
-
 }
 
 /*
@@ -93,10 +92,10 @@ function ou_digital_futures_menu_tree__menu_block__main_menu($variables)
         foreach ($headerLinksArray as $key=>$value) {
           // If the Theme setting matches this heading then add in the sub menu
           if ($key == theme_get_setting('ou_df_colour_scheme')) {
-            $insert_menu .= '<li class="'.$key.'"><a href="'.$value['a_href'].'"><span>'.$value['title'].'</span></a><ul>' . $variables['tree'] . '</ul></li>';
+            $insert_menu .= '<li class="'.$key.'"><a href="'.$value['a_href'].'"><span class="ou-page-courses">'.$value['title'].'</span></a><ul>' . $variables['tree'] . '</ul></li>';
           }
           else {
-            $insert_menu .= '<li class="'.$key.'"><a href="'.$value['a_href'].'"><span>'.$value['title'].'</span></a></li>';
+            $insert_menu .= '<li class="'.$key.'"><a href="'.$value['a_href'].'"><span class="ou-page-courses">'.$value['title'].'</span></a></li>';
           }
 
         }
